714d41663a475252c3994711ee167beb383354bf
Timotheus Beirer <timotheus.beirer@iu-study.org>
2026-05-25 12:17:25 +0200

    Begruessungs_effekt_in_arbeit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 document.addEventListener('DOMContentLoaded',() => {
-    const options = {
-        strings: ['Ciao!','Hello!','Hola!','Bonjour!'],
-        typeSpeed: 100,
-        backSpeed: 50,
-        loop: true
-    };
-    const typed = new Typed('#greeting-typewriter', options);
-});
+
+// Begrüßung nach Zeit des Tages
+const currentHour = new Date().getHours();
+let Begruessung = "Ciao!"; // Standardbegrüßung
+
+if (currentHour >= 5 && currentHour < 12) {
+    Begruessung = "Guten Morgen!";
+} else if (currentHour >= 12 && currentHour < 18) {
+    Begruessung = "Guten Tag!";
+} else if (currentHour >= 18 && currentHour < 23) {
+    Begruessung = "Gute Abend!";
+}
+
+// Tipp_Effekt
+if (document.getElementById('greeting-typewriter')) {
+    new Typed('#greeting-typewirter', {
+        strings: [Begruessung],
+        typeSpeed: 80,
+        showCursor: true,
+        cursorChar: '|',
+        loop: false 
+    });     
+}
+
