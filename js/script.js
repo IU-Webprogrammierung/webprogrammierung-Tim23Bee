d12e0841febdaabeebbc4de1a1d13fa5e1b4cdca
Timotheus Beirer <timotheus.beirer@iu-study.org>
2026-06-05 16:38:15 +0200

    Ergänze_footer.html//header_entfernt

--- js/script.js
+++ js/script.js
@@ -31,6 +31,21 @@ if (document.getElementById('greeting-typewriter')) {
 // =============================================== //
 
 
-//=============================================== //
-// /about.html Tipp_Effekt //
 
+
+// =============================================== //
+// /komponenten header und footer laden //  
+
+loadComponents("header","components/header.html");
+loadComponents("footer","components/footer.html");
+
+async function loadComponents(selector, path){  
+
+   const response = await fetch(path);
+   const html = await response.text(); 
+   const element = document.querySelector(selector);
+   element.innerHTML = html;
+
+}
+
+// =============================================== //
